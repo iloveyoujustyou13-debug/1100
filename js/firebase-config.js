8e8cb1ce635385891ecd858bf7fe4ada3f8e438a
iloveyoujustyou13-debug <iloveyoujustyou13@gmail.com>
2026-07-07 17:41:54 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,25 @@
+// FILE: js/firebase-config.js
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// .env ফাইল থেকে কনফিগ ভ্যালু গুলো লোড করুন
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  client_id: '635746561853-62pjvjs4mt6hjh4dspl8on1uo5pcbejj.apps.googleusercontent.com'
+});
+
+export const ADMIN_EMAIL = 'gamingmanojit14@gmail.com';
